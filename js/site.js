9d1cd342db2c30b2a931305788d0bb822314d3ea
NickSeyler <nickseyler25@gmail.com>
2022-01-19 15:24:27 -0500

    Init

--- js/site.js
+++ js/site.js
@@ -1,9 +1,47 @@
-//Display message to the user
-function getMessage(){
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'The App Title',
-        userMessage,
-        'success'
-    );
+//get the string from the user
+function getInput() {
+    let userStr = document.getElementById("userStr").value;
+
+    let isPalindrome = checkPalindrome(userStr);
+
+    displayData(isPalindrome);
+}
+
+//Input: String
+//Return: Boolean
+//Check for palindrome
+function checkPalindrome(str) {
+    let modifiedStr = "";
+    let revStr = "";
+
+    //remove special characters
+    for (let i = 0; i < str.length; i++){
+        if ((/[0-9a-zA-Z]/).test(str[i])){
+            modifiedStr += str[i].toLowerCase();
+        }
+    }
+
+    for (let i = modifiedStr.length - 1; i >= 0; i--) {
+        revStr += modifiedStr[i];
+    }
+
+    return revStr == modifiedStr;
+}
+
+//Input: Boolean
+//Return: None
+//Display whether or not the item is a palindrome
+function displayData(isPalindrome) {
+
+    if (isPalindrome){
+        document.getElementById("alertPalindromeMessage").innerHTML = `Yes, it's a palindrome!`;
+        document.getElementById("alertPalindrome").classList.remove("alert-danger");
+        document.getElementById("alertPalindrome").classList.add("alert-success");
+    }
+    else{
+        document.getElementById("alertPalindromeMessage").innerHTML = `No, it's not a palindrome!`;
+        document.getElementById("alertPalindrome").classList.remove("alert-success");
+        document.getElementById("alertPalindrome").classList.add("alert-danger");
+    }
+    document.getElementById("alertPalindrome").classList.remove("d-none");
 }
